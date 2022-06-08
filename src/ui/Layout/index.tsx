46c363a2235be894b0152aa0a76649d263a967ed
Vladimir Shadrov <shadrov@web-creator.ru>
2022-06-08 10:53:22 +0300

    update layout

--- src/ui/Layout/index.tsx
+++ src/ui/Layout/index.tsx
@@ -5,7 +5,6 @@ type Props = {
   children?: React.ReactNode;
 };
 
-const Layout: FC<Props> = ({ children }) => {
+export const Layout: FC<Props> = ({ children }) => {
   return <div className={styles.layout}>{children}</div>;
 };
-export default Layout;
