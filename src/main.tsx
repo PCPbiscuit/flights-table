50fab8b4b253a9bcbd4b247440df1259db3c4a55
Vladimir Shadrov <vovashadrov@yandex.ru>
2022-06-22 10:50:44 +0300

    add rrd dep and refactor main accordingly

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { QueryClientProvider } from 'react-query';
+
 import { createQueryClient } from './api';
 import App from './app/App';
 import './index.css';
@@ -10,8 +12,12 @@ const queryClient = createQueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <Routes>
+          <Route path="/" element={<App />} />
+        </Routes>
+      </QueryClientProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 );
